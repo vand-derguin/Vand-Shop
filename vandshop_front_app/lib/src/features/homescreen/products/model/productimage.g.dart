d1710b273d9ce5759c4a75dd1d16e1b79c64a437
vand-derguin <djonar20@gmail.com>
2025-08-27 09:21:25 +0100

    mix

---
+++ vandshop_front_app/lib/src/features/homescreen/products/model/productimage.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'productimage.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$ProductimageImpl _$$ProductimageImplFromJson(Map<String, dynamic> json) =>
+    _$ProductimageImpl(
+      url: json['url'] as String,
+    );
+
+Map<String, dynamic> _$$ProductimageImplToJson(_$ProductimageImpl instance) =>
+    <String, dynamic>{
+      'url': instance.url,
+    };
--- vandshop_front_app/lib/src/features/homescreen/products/repository/product_repository.dart
+++ vandshop_front_app/lib/src/features/homescreen/products/repository/product_repository.dart
@@ -1,6 +1,15 @@
-import 'package:vandshop_front_app/src/core/network/dio_service.dart';
+// import 'package:dartz/dartz.dart'
+// import 'package:vandshop_front_app/src/core/network/dio_service.dart';
+// import 'package:dio/dio.dart';
+// import 'package:vandshop_front_app/src/core/network/error_handler.dart';
+// import 'package:vandshop_front_app/src/features/homescreen/products/products.dart';
 
-class ProductRepository {
-  final DioService dio;
-  ProductRepository({required this.dio}); // Dependency Injection
-}
+
+// class ProductRepository {
+//   final DioService dio;
+//   ProductRepository({required this.dio});
+  
+//   //FETCH ALL PRODUCTS
+//   //FETCH SINGLE PRODUCTS
+//   //FETCH  PRODUCTS BY CATEGORY ID
+// }
